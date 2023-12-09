70926459c49521dbbaa86b7e35597e6f28df7dae
lenguyen17 <lenguyencm97@gmail.com>
2023-12-09 16:10:24 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+class Calculator {
+    constructor() { }
+
+    addT(a, b) {
+        return a + b;
+    }
+
+    multiple(a, b) {
+        return a * b;
+    }
+
+    minus(a, b) {
+        return a - b;
+    }
+
+    divide(a, b) {
+        return a / b;
+    }
+}
+
+var cal = new Calculator();
+console.log(cal(1,2));
