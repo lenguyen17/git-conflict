66d599e8ec2d300921b4580bbea1ebc4706e065e
lenguyen17 <lenguyencm97@gmail.com>
2023-12-10 15:28:50 +0700

    return

---
+++ pages/cal.js
@@ -0,0 +1,17 @@
+function add(a,b){
+    return a + b;
+}
+
+function minus (a, b){
+    return a - b;
+}
+
+function multiple(a,b){
+    return a * b;
+}
+
+// from feature 1
+
+function divide(a,b){
+    return a / b;
+}
